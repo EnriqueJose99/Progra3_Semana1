705f8e8d40d1d3e670cbbba3de6b76c423d568ad
EnriqueJose99 <enrique.galeano99@yahoo.com>
2018-01-24 08:01:48 -0600

    operaciones y booleano

--- Semana1.cpp
+++ Semana1.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 
 int main(){
-	int array[5];
+	/*std es namespace e incluye objetos
+	el getline lee toda la linea, lee de la consola y cuando encuentra un salto 
+	de linea se detiene*/
+
+	/*int array[5];
 	cout<<"Escriba el valor de cinco enteros" <<endl;
 
 	for(int i =0; i<5;i++){
@@ -15,8 +19,25 @@ int main(){
 	for(int i= 0;i < 5;i++){
 		cout<< array[i] << " ";
 	}
-	return 0;
-}
+	return 0;*/
 
-/*std es namespace e incluye objetos
-el getline lee toda la linea, lee de la consola y cuando encuentra un salto delinea se detiene*/
+	/*Tipos de Datos enteros en java y c++ int, long, short, byte, en c++ ha y dos mas que son el char y bool*/
+	
+	/*int num = 'a';//casteo implicito
+	char letra = 70;//casteo implicito
+	cout<<"Numero: "<< num <<endl << "Char: "<< letra <<endl;*/
+	
+
+	
+	//el booleano solo es falso cuando es 0 
+	//toda operacion "if" return algo "el valor que le asigno a la variable"
+	//Mucho cuidado por que el compilador de c++ no mira la logica de comparar o asignar
+	
+	bool condition = true;
+	if(condition){
+		cout<<"Verdadero"<< endl;
+	}else{ 
+	        cout<<"False"<< endl;
+	}
+
+}
