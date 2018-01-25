d787609f5fc8f16eb312d5fe949c5e337a1bfc35
EnriqueJose99 <enrique.galeano99@yahoo.com>
2018-01-25 14:36:04 -0600

    Semana 1 Apuntadores

---
+++ Semana1Apuntadores.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+	/*Un apuntador es un tipo de variable que tiene nombre, direccion de 
+	 memoria, dirrecion de la variable. El valor contiene una direccion */
+
+
+
+}
